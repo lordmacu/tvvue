5fb4e53b440a6a970c1cbf10947f8ace46c967df
Crsitian Camilo Garcia <informacion@cristiangarcia.co>
2019-01-07 17:14:53 -0300

    fix asset

--- src/view/pages/Home/index.vue
+++ src/view/pages/Home/index.vue
@@ -19,11 +19,11 @@
                             </tr>
                             <tr>
                                 <td><button v-on:click="jumpStep(2)" >Izquierda</button></td>
-                                <td class="container-play"><button class="play-button" v-on:click="jumpUp()" >Play</button></td>
+                                <td class="container-play"><button class="play-button" v-on:click="loadVideo()" >Play</button></td>
                                 <td><button v-on:click="jumpStep(1)" > Derecha</button></td>
                             </tr>
                             <tr>
-                                <td></td>
+                                <td> <button v-on:click="back()"><- Atras</button></td>
                                 <td> <button v-on:click="jumpDown()">Abajo</button></td>
                                 <td></td>
                             </tr>
@@ -39,8 +39,17 @@
                         <span>Duración - {{preview.infoTime}}</span> - 
                         <span>Calidad - {{preview.infoQlty}}</span> 
                         <p v-html="preview.description"></p>
-                    </div>
+                   <br/>
+                        <div class="row">
+                             <div class="col-12 container-button-video" v-for="source in sourcesVideo">
 
+                            <button class="btn btn-default  btn-light" v-on:click="showVideoIdentifier(source.identifier)">
+                                {{source.nameResource}}
+                            </button>
+                        </div>
+                        </div>
+                        
+                    </div>
 
 
                 </div>
@@ -48,26 +57,63 @@
 
             <div class="col-lg-9">
                 <div  id="movies-container"  nv-scope="menu" class="row movies-container" nv-scope-current v-if='movies.length>0'>
+                    
+                    <div class="col-12 container-categories">
+                        <div class="row">
+                            <div class="col-7">
+                                <button class="btn btn-default btn-light" v-on:click="latest()" >Últimas</button>
+                        <button class="btn btn-default btn-light" v-on:click="newMovies()">Estrenos</button>
+                        <button class="btn btn-default btn-light" v-on:click="ranking()">Ranking</button>
+                        <button class="btn btn-default btn-light" v-on:click="popular()">Mas vistas</button>
+                        
+                            </div>
+                            
+                            <div class="col-5">
+                                <div class="input-group mb-3">
+                                    <input type="text" v-model="searchText" class="form-control" placeholder="Buscar por nombre">
+
+                                    <div class="input-group-prepend">
+                                      <button class="btn btn-outline-secondary" type="button" id="button-addon1" v-on:click="searchMovie">Buscar</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        
+                  
+                    </div>
                     <div nv-el  nv-el-current :class="checkZoom(movies[0])" v-popover:tooltip="'This is a string value'"  v-on:click="overClick(movies[0],0)"  :name="'element_'+0" :id="'element_'+0" :datai="0"  :style="'background-image:url('+movies[0].image+')'" >
 
                          <div class="content-movie row"  >
-                            ss  {{movies[0].current}}
+                            {{movies[0].infoQlty}}
                         </div>
 
                     </div>
 
 
-                    <div v-for="(movie,index) in movies"  :class="checkZoom(movie)" :name="'element_'+index" v-popover:tooltip="'This is a string value'"  v-on:click="overClick(movie,index)"  v-if="index!=0"  :id="'element_'+index"    :datai="index"   nv-el  class="col-2 movie-container" :style="'background-image:url('+movie.image+')'" >
+                    <div v-for="(movie,index) in movies"  :class="checkZoom(movie)" :name="'element_'+index"  v-on:click="overClick(movie,index)"  v-if="index!=0"  :id="'element_'+index"    :datai="index"   nv-el  class="col-2 movie-container" :style="'background-image:url('+movie.image+')'" >
                         <div class="content-movie row ss">
-                            ss {{movie.current}}
+                            {{movie.infoQlty}}
                         </div>
-
-
-
-
-
+                    </div>
+                    
+                    <div class="col-12 load-more-container">
+                        <button class="btn btn-default btn-light" v-on:click="loadMore">Cargar mas</button>
                     </div>
 
+
+
+                </div>
+
+                <div class="container-video " v-if="frameVideo">
+                    <div class="row">
+                       
+                    <div class="col-12 container-video-frame" >
+                        <iframe class="iframex no-you" width="100%" height="500" :data-src="frameVideo" :src="frameVideo" frameborder="0" allowfullscreen=""></iframe>
+                    </div>
+                    </div>
+                    
+                   
                 </div>
             </div>
         </div>
@@ -90,7 +136,13 @@
                 movies: [],
                 preview: null,
                 jumpValue: 0,
-                jumpStepValue: 0
+                jumpStepValue: 0,
+                sourcesVideo: [],
+                players:[],
+                frameVideo:null,
+                paginator:1,
+                category:0,
+                searchText:null
             }
         },
         created() {
@@ -145,9 +197,101 @@
 
 
 
+                this.paginator=1;
 
+            this.loadHome();
 
-            this.axios.get("https://alquilerdirecto.com.ar/getHomeMovies", ).then((response) => {
+            console.log("aqui mano", $);
+        },
+        computed: {
+
+        },
+        methods: {
+            searchMovie(){
+                
+                this.paginator=1;
+                    this.movies=[];
+                
+                if(!this.searchText){
+                    this.category=0;
+
+                    this.loadHome();
+                    return false;
+                }
+                 this.movies=[];
+                 this.axios.get("https://www.alquilerdirecto.com.ar/searchMovie?search="+this.searchText, ).then((response) => {
+
+                var responseHtml = $(response.data);
+                var listMovies = responseHtml.find(".MovieList li");
+
+                for (var i = 0; i < listMovies.length; i++) {
+                    var item = $(listMovies[i]);
+                    var link = item.find("a").attr("href");
+                    var title = item.find(".Title").html();
+                    var year = item.find(".Year").html();
+                    var image = item.find("figure img").attr("src").replace("-160x242", "");
+                    var image = image.replace("-55x85", "");
+                    var infoTime = item.find(".Info .Time").html();
+                    var infoQlty = item.find(".Info .Qlty").html();
+                    var description = item.find(".Description").html();
+
+                    this.movies.push({
+                        link: link,
+                        title: title,
+                        year: year,
+                        image: image,
+                        infoTime: infoTime,
+                        infoQlty: infoQlty,
+                        description: description,
+                        current: false,
+                        index: i
+                    });
+
+                }
+
+                this.preview = this.movies[0];
+
+                setTimeout(function () {
+                    navigation.refresh();
+
+                }, 1000);
+
+                });
+            },
+            
+            latest(){
+                    this.paginator=1;
+                    this.movies=[];
+                this.category=0;
+                this.loadHome();
+            },
+            newMovies(){
+                                    this.movies=[];
+
+                this.paginator=1;
+                this.category="estrenos";
+                this.loadHome();
+            },
+            ranking(){
+                                    this.movies=[];
+
+                    this.paginator=1;
+                this.category="peliculas-mas-valoradas";
+                this.loadHome();
+            },
+            popular(){
+                                    this.movies=[];
+
+                    this.paginator=1;
+                this.category="peliculas-mas-vistas";
+                this.loadHome();
+            },
+            
+            
+            
+            loadHome(){
+              
+            this.axios.get("https://alquilerdirecto.com.ar/getHomeMovies?paginator="+this.paginator+"&category="+this.category, ).then((response) => {
 
                 var responseHtml = $(response.data);
                 var listMovies = responseHtml.find(".MovieList li");
@@ -187,14 +331,86 @@
 
                 console.log(this.movie);
 
-            })
+            })  
+            },
+            back(){
+                this.frameVideo=null;
+            },
+            loadMore(){
+                this.paginator++;
+                this.loadHome();
+            },
 
-            console.log("aqui mano", $);
-        },
-        computed: {
+            loadVideo() {
+                this.loadUrl(this.preview.link);
+            },
+            
+            getVideoFrame(url){
+                this.frameVideo=url;
+                
+                return false;
+                var url=url.split("file=");
+                if(url.length==2){
+                this.frameVideo=url[1];
+                }else{
+                this.frameVideo=url[0];
+                }
+                /*this.axios.get("https://alquilerdirecto.com.ar/getUrl?url=" + encodeURI(url), ).then((response) => {
 
-        },
-        methods: {
+                });*/
+            },
+            loadUrl(url) {
+
+
+
+                this.axios.get("https://alquilerdirecto.com.ar/getUrl?url=" + encodeURI(url), ).then((response) => {
+                    var responseHtml = $(response.data);
+                    
+                    var players=responseHtml.find(".TPlayerTb");
+                    
+                    for (var ifr = 0; ifr < players.length; ifr++) {
+
+                        var iframe = $(players[ifr]);
+                        
+                        this.players.push({id:iframe.attr("id"),frame:iframe.find("iframe").attr("data-src")})
+                        }
+                        console.log(this.players);
+
+
+                    
+                    var videosResources = responseHtml.find(".open_submenu ul li");
+                    //    var resource=$(videosResources[i]);
+                    this.sourcesVideo=[];
+                    for (var i = 0; i < videosResources.length; i++) {
+
+                        var resource = $(videosResources[i]);
+                        var identifier = resource.attr("data-tplayernv");
+                        var nameResource = resource.find("span span").html();
+                        var image = resource.find("img").attr("src");
+
+                        this.sourcesVideo.push({identifier: identifier, nameResource: nameResource, image: image});
+
+
+                    }
+                    
+                    
+                    this.showVideoIdentifier(this.sourcesVideo[0].identifier);
+
+
+
+                })
+            },
+            showVideoIdentifier(source){
+              
+                    for (var i = 0; i < this.players.length; i++) {
+                        if(this.players[i].id==source){
+                               // this.frameVideo=this.players[i].frame;
+                                
+                                this.getVideoFrame(this.players[i].frame);
+                        }
+                }
+              
+            },
             jumpStep(step) {
                 if (step == 1) {
 
