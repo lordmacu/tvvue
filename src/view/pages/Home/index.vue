8e8a91dd38ca90722fdffd72d87d4e2d8b4e846e
Crsitian Camilo Garcia <informacion@cristiangarcia.co>
2019-01-07 18:02:57 -0300

    fix asset

--- src/view/pages/Home/index.vue
+++ src/view/pages/Home/index.vue
@@ -232,19 +232,18 @@
                 }
             },
             fullscreen(){
-                this.fullscreenI=true;
-               
-    var elem = document.getElementById("myvideo");
- if (elem.requestFullscreen) {
+              //  this.fullscreenI=true;
+               var elem = document.documentElement;
+
+if (elem.requestFullscreen) {
     elem.requestFullscreen();
   } else if (elem.mozRequestFullScreen) { /* Firefox */
     elem.mozRequestFullScreen();
-  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
     elem.webkitRequestFullscreen();
   } else if (elem.msRequestFullscreen) { /* IE/Edge */
     elem.msRequestFullscreen();
   }
- 
         
         
             },
