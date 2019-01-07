bff5d617084abed1f174295d7757ac17835c6aeb
Crsitian Camilo Garcia <informacion@cristiangarcia.co>
2019-01-07 18:43:35 -0300

    fix asset

--- src/view/pages/Home/index.vue
+++ src/view/pages/Home/index.vue
@@ -161,15 +161,7 @@
             
          document.onkeydown = function(evt) {
     evt = evt || window.event;
-    var isEscape = false;
-    if ("key" in evt) {
-        isEscape = (evt.key == "Escape" || evt.key == "Esc");
-    } else {
-        isEscape = (evt.keyCode == 27);
-    }
-    if (isEscape) {
-        alert("Escape");
-    }
+    alert(evt.keyCode);
 };   
             /*
              setInterval(() => {
